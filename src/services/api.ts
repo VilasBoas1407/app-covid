c1db7e7548bbe62232ab30a101fee483607515b8
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-07-17 19:06:28 -0300

    add production api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/'
+    baseURL: 'http://stopcovidabrasel-com-br.umbler.net/'
 });
 
 export default api;
