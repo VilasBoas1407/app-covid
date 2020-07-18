21a0dbfa0bc9c5dd8e08704e7f3796d1cdd68a90
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-07-17 22:08:17 -0300

    add build folder

---
+++ build/precache-manifest.b0b5a9e1ccdea0d9ffa556f39e6c37ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d2a2fbf0fa247be4ee73622f453ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5103801acd54cbe1413b",
+    "url": "/static/css/main.292018f0.chunk.css"
+  },
+  {
+    "revision": "957d6a803cf15b07b4e3",
+    "url": "/static/js/2.078c7d1b.chunk.js"
+  },
+  {
+    "revision": "62d1e3b8cd151c4743f4a4b9b413b791",
+    "url": "/static/js/2.078c7d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5103801acd54cbe1413b",
+    "url": "/static/js/main.907aad02.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "8f7a83af5b20bff213c768430bacb184",
+    "url": "/static/media/home-background.8f7a83af.png"
+  },
+  {
+    "revision": "7b17c987c60cdbeb0432a79208743bc8",
+    "url": "/static/media/login.7b17c987.png"
+  },
+  {
+    "revision": "3ede47506c15a3510c4888871fc10815",
+    "url": "/static/media/logo.3ede4750.png"
+  }
+]);
