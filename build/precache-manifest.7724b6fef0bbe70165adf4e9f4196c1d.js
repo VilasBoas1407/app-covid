08ee8d99d6a156cd9fd6d7b5c6de80c59a6ec8d8
Jose Claudio <jjoclas@hotmail.com>
2020-07-20 20:16:49 -0300

    alter date format on followup

---
+++ build/precache-manifest.7724b6fef0bbe70165adf4e9f4196c1d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b1c233b4d778558f1e22aefeed39bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d64d24cd83bf4db76b08",
+    "url": "/static/css/main.f9fab09f.chunk.css"
+  },
+  {
+    "revision": "ce13cb955dae4c7cfef2",
+    "url": "/static/js/2.615fcecb.chunk.js"
+  },
+  {
+    "revision": "62d1e3b8cd151c4743f4a4b9b413b791",
+    "url": "/static/js/2.615fcecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64d24cd83bf4db76b08",
+    "url": "/static/js/main.3d967075.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "97b452e4c9b5f0043f6894959ad27508",
+    "url": "/static/media/home-background.97b452e4.png"
+  },
+  {
+    "revision": "7b17c987c60cdbeb0432a79208743bc8",
+    "url": "/static/media/login.7b17c987.png"
+  },
+  {
+    "revision": "3ede47506c15a3510c4888871fc10815",
+    "url": "/static/media/logo.3ede4750.png"
+  }
+]);
