b84901193900a9edb3907512fdfbb3b813337df6
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2020-08-01 17:17:12 -0300

    emitir relatório

--- src/components/Dashboard/HeaderUsers/index.js
+++ src/components/Dashboard/HeaderUsers/index.js
@@ -13,25 +13,27 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import PopUp from './DialogPopUp/index'
 import api from '../../../services/api'
-import ReactDOM from 'react-dom';
-import { PDFViewer } from '@react-pdf/renderer';
 import PictureAsPdfIcon from '@material-ui/icons/PictureAsPdf';
 
-import MyDocument from '../../PDF';
+import './styles.css';
 
 export default function THeaderUser(props){
 
 
   const[open,setOpen] = useState(false);
+  const[openRelatorio,setOpenRelatorio] = useState(false);
   const [userPopUp , setUser] = useState()
   const [userName , setName] = useState('Nome:')
+  const [userData,setUserData] = useState({
+    nome_empresa : ''
+  });
+
 
   const { rows } = props
 
   async function getPopData(id){
     
     const token = await localStorage.getItem("token");
-    
     api.request({
       method: 'GET',
         url: `/followup`,
@@ -58,6 +60,31 @@ export default function THeaderUser(props){
     setOpen(false);
   }
 
+  async function handlePDFOpen(user){
+
+    const EmpData = await JSON.parse(localStorage.getItem("userData"));
+    console.log(user);
+    let userData = {};
+    
+    userData.nome = user[2];
+    userData.telefone = user[3];
+    userData.email = user[4];
+    userData.nome_empresa = EmpData.ds_nome;
+    console.log(userData);
+    await setUserData(userData);
+
+    setOpenRelatorio(true);
+
+  }
+
+  function handlePDFPrint(){
+      var content = document.getElementById("relatorio");
+      window.print(content);
+  }
+  function handlePDFClose(){
+      setOpenRelatorio(false);
+  }
+
     
     return(
         <Table size="small">
@@ -81,7 +108,7 @@ export default function THeaderUser(props){
               </TableCell>
               <TableCell>
                 <IconButton color="primary" aria-label="Exportar dados para PDF">
-                  <PictureAsPdfIcon />
+                  <PictureAsPdfIcon onClick={() =>{ getPopData(row[0], setName('Nome: '+row[2]), handlePDFOpen(row))}}/>
                 </IconButton>
               </TableCell>
             </TableRow>
@@ -104,6 +131,36 @@ export default function THeaderUser(props){
                 Fechar
               </Button>
             </DialogActions>
+          </Dialog>
+            {/*  ----- Modal PDF ----- */}
+          <Dialog
+            open={openRelatorio}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+                <DialogTitle id="alert-dialog-title">
+                  Relatório - {userData.nome_empresa}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                      <label>Nome: {userData.nome}</label><br/><br/>
+                      <label>Telefone : {userData.telefone}</label><br/><br/>
+                      <label>E-mail: {userData.email}</label>
+                      <br/><br/>
+                      <PopUp rows = { userPopUp}/>
+                  </DialogContentText>
+                </DialogContent>  
+              <div className="no-printme" >
+              <DialogActions>
+                <Button onClick={handlePDFPrint} color="primary" autoFocus>
+                  Imprimir
+                </Button>
+                <Button onClick={handlePDFClose} color="primary" autoFocus style={{ print: 'none'}}>
+                  Fechar
+                </Button>
+              </DialogActions>
+              </div>
           </Dialog>
         </Table>
 
